cfcad7e02bf4dfc82455d2baed7bf260856b948c
Jeff Young <cwmoo740@gmail.com>
2017-05-30 17:07:03 -0400

    add comment explaining issue

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import { createSelector } from 'reselect';
+/*
+ * changing the import to
+ * import * as reselect from 'reselect';
+ * and accessing reselect.createSelector fixes the issue
+*/
 export interface State {
     a: string;
     b: number;
